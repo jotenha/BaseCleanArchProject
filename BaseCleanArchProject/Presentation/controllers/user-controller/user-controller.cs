6e64f169fe42c3f96b580aea2b2a36b6564f5b34
Pedro Savian <jotahpeee@live.com>
2022-06-28 19:27:17 -0300

    chore: added user controller exemple

---
+++ BaseCleanArchProject/Presentation/controllers/user-controller/user-controller.cs
@@ -0,0 +1,25 @@
+﻿using BaseCleanArchProject.Domain.models;
+using BaseCleanArchProject.Infra.repos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseCleanArchProject.Presentation.controllers.user_controller
+{
+    internal class User_controller
+    {
+        public User_controller()
+        {
+        }
+
+        public Domain.models.User Signup (string id, string name, string email, string password)
+        {
+            UserRepo user = new UserRepo(id, name, email, password);
+            Domain.models.User addedUser = user.Add();
+            return addedUser;
+        }
+    }
+}
