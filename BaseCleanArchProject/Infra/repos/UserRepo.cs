eb442acc7d5a949525f95f083c10dc3e241d99cd
Pedro Savian <jotahpeee@live.com>
2022-06-28 19:15:54 -0300

    chore: added infra user exemple

---
+++ BaseCleanArchProject/Infra/repos/UserRepo.cs
@@ -0,0 +1,30 @@
+﻿using BaseCleanArchProject.Domain;
+using BaseCleanArchProject.Domain.models;
+using BaseCleanArchProject.Domain.usecases.User_usecases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseCleanArchProject.Infra.repos
+{
+    internal class UserRepo : User, Iadd_user, Iremove_user
+    {
+        public UserRepo(string id, string name, string email, string password) : base(id, name, email, password)
+        {
+        }
+
+        public User Add(User user)
+        {
+            //Database add
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(User user)
+        {
+            //Database remove
+            throw new NotImplementedException();
+        }
+    }
+}
